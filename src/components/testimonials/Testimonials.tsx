7df322e3df9eebd4899bfc3625dee1592d628964
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2024-12-25 19:14:14 +0200

    update testimonials

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -73,7 +73,7 @@ export default function Testimonials() {
                   <div className="slider-box-desc">
                     <p>
                       Excellent service! Highly recommended for professional
-                      work and great customer care.
+                      work 
                     </p>
                   </div>
                 </div>
