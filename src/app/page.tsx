01db3e453fc7848d2d90aec0825c02260db09224
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2024-12-25 20:36:26 +0200

    update sectons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Fix2 from "@/components/fix2/Fix2";
 import Hero from "@/components/hero/Hero";
 import MoreAbout from "@/components/more-about/MoreAbout";
 import Testimonials from "@/components/testimonials/Testimonials";
+import Map from "@/components/map/Map";
+import Asked from "@/components/asked/Asked";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <MoreAbout />
       <Fix2   />
       <Awards />
+      <Map />
+      <Asked  />
     </>
   );
 }
