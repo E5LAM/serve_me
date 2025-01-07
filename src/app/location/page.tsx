3209341c7f13c0cd84cde4ebd4147cb11fbaa83b
MuhammedFayad <92796091+Nyctophilus@users.noreply.github.com>
2025-01-07 16:21:53 +0200

    location page

---
+++ src/app/location/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { Container } from "react-bootstrap";
+import Hero from "@/components/hero/Hero";
+import MoreAbout from "@/components/more-about/MoreAbout";
+import Testimonials from "@/components/testimonials/Testimonials";
+import TextSectionWithButton1 from "@/app/location/TextSectionWithButton";
+import TextSectionWithButton2 from "./TextSectionWithButton2";
+import TextSectionWithButton3 from "./TextSectionWithButton3";
+
+export const metadata: Metadata = {
+  title: "أخدمنى | الموقع",
+  description: "موقع اخدمنى لخدمات التنظيف",
+};
+
+export default function LocationPage() {
+  return (
+    <main className="location">
+      <Hero />
+      <TextSectionWithButton1 />
+
+      <section className="testimonials">
+        <Container>
+          <Testimonials md={undefined} />
+        </Container>
+      </section>
+
+      <MoreAbout />
+
+      <TextSectionWithButton2 />
+      <TextSectionWithButton3 />
+    </main>
+  );
+}
