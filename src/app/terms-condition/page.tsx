3b8198bf4b62fa510256669c626ca0bb7e731c7b
Yousef Ahmed <floky4444@gmail.com>
2025-02-04 20:23:53 +0200

    main

--- src/app/terms-condition/page.tsx
+++ src/app/terms-condition/page.tsx
@@ -1,31 +1,43 @@
-"use client";
-import { AxiosInstance } from "@/Api/Api";
-import { useEffect, useState } from "react";
+import Footer from "@/components/layout/Footer";
 import { Container } from "react-bootstrap";
 
-export default function Page() {
-  const [terms, setTerms] = useState("");
+async function getTermsData() {
+  try {
+    const res = await fetch(`${process.env.BASE_URL}/terms`);
 
-  useEffect(() => {
-    const handleFetch = async () => {
-      try {
-        const response = await AxiosInstance.get("terms");
-        console.log(response.data.data.description);
-        setTerms(response.data.data.description);
-      } catch (error) {
-        console.error("Error fetching terms:", error);
-      }
+    if (!res.ok) throw new Error("Failed to fetch terms data");
+
+    const data = await res.json();
+
+    return {
+      terms: data.data.description || "",
+      footer: data.data.footer || null,
+      footersquares: data.data.footersquares || [],
     };
-    handleFetch();
-  }, []);
+  } catch (error) {
+    console.error("Error fetching terms data:", error);
+    return {
+      terms: "",
+      footer: null,
+      footersquares: [],
+    };
+  }
+}
+
+export default async function Page() {
+  const { terms, footer, footersquares } = await getTermsData();
+
   return (
-    <section className="privacy-policy">
-      <div className="privacy-header text-center">
-        <h2 className="mb-5">الشروط والأحكام</h2>
-      </div>
-      <div className="privacy-body">
-        <Container dangerouslySetInnerHTML={{ __html: terms }} />
-      </div>
-    </section>
+    <>
+      <section className="privacy-policy">
+        <div className="privacy-header text-center">
+          <h2 className="mb-5">الشروط والأحكام</h2>
+        </div>
+        <div className="privacy-body">
+          <Container dangerouslySetInnerHTML={{ __html: terms }} />
+        </div>
+      </section>
+      {footer && <Footer footerData={footer} footebox={footersquares} />}
+    </>
   );
 }
