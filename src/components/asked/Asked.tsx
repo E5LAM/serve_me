651ab59aa87dc95bc4346158957ef3e099a319c6
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-06 16:51:25 +0200

    update css

--- src/components/asked/Asked.tsx
+++ src/components/asked/Asked.tsx
@@ -1,19 +1,19 @@
 import { Container } from "react-bootstrap";
-import QuestionsAccordion from "./QuestionsAccordion";
+// import QuestionsAccordion from "./QuestionsAccordion";
 
 export default async function Asked() {
-  const res = await fetch(`${process.env.BASE_URL}/faqs`);
-  const data = await res.json();
+  // const res = await fetch(`${process.env.BASE_URL}/faqs`);
+  // const data = await res.json();
 
   return (
     <section className="asked">
       <Container>
         <h2 className="text-center my-5">الأسئلة المتكررة حول خدمات التنظيف</h2>
-        {data?.data ? (
+        {/* {data?.data ? (
           <QuestionsAccordion items={data.data} />
         ) : (
           <p>غير متوفر أسئلة حاليا...</p>
-        )}
+        )} */}
       </Container>
     </section>
   );
